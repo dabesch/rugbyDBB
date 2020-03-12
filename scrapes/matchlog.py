28c8878964cfb0bacb6676587bc6f5e06d1f675d
Dabesch <dhabesch@googlemail.com>
2020-03-12 02:00:26 +0000

    Complete all functions to write to db, wrap in scrape()

--- scrapes/matchlog.py
+++ scrapes/matchlog.py
@@ -1,25 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
 from io import StringIO
+
 import pandas as pd
+from bs4 import BeautifulSoup
+
+from database import createSQL, executeQuery, writeDataFrame
 
 
 def readNotes(notes, matchID):
-    notesDict = {'matchID': matchID}
-    teams = ['Home', 'Away']
-    tid = 0
+    """
+    :param notes: Soup object which contains results from the 'Notes' tab
+    :param matchID: the match id
+    :return: returns a complete dictionary of the notes from the match
+    """
+    notesDict = {'match_id': matchID}
+
     for n in notes:
         res = n.text.strip().split('\n')
         if len(res) > 1:
-            title = res[0]
+            title = res[0].replace(' ', '_')
             if 'debuts' in title:
-                title = f'{teams[tid]} debuts'
-                tid += 1
+                continue
             values = "".join(res[1:])
-            if title == 'Referee' or title == 'Ground name':
+            if title == 'Referee' or title == 'Ground_name':
                 href = n.a['href'].split('/')[-1].split('.')[0]
-                notesDict[f'{title}ID'] = int(href)
+                notesDict[f'{title}_id'] = int(href)
             notesDict[title] = values
+        elif 'Attendance' in res[0]:
+            notesDict['attendance'] = res[0].split(' ')[-1]
+
     return notesDict
 
 
@@ -44,6 +52,7 @@ def readTeams(teams):
     return teamDict
 
 
+# todo not yet used but should be used with readTeams above
 def parseScore(string, matchID, nation, sType):
     scD = []
     for sc in string.split('),'):
@@ -59,10 +68,15 @@ def parseScore(string, matchID, nation, sType):
     return scD
 
 
-def matchStats(matchStats):
-    teams = matchStats.find_all('td', {'class': 'liveMthHdrGrn'})
-    teams = [t.text for t in teams]
+def matchStats(matchStats, teamScores, matchID):
+    """
+    :param matchStats: soup object which contains the match stats for the overall game
+            (separate from individual team stats)
+    :return: a dictionary object which is then cleaned and converted to a DataFrame with processMatchStats()
+    """
+    teams = [teamScores[t]['team'] for t in ['home', 'away']]
     stats = {'team': teams, 'home': [1, 0]}
+
     for m in matchStats.find_all('tr'):
         if len(m.find_all('td', {'class': 'liveTblTextGrn'})) == 1:
             title = m.find('td', {'class': 'liveTblTextGrn'}).text
@@ -70,15 +84,21 @@ def matchStats(matchStats):
             txt = [b.text.strip() for b in blktxt]
 
             stats[title] = txt
-    return processTeamStats(stats)
+    return processMatchStats(stats, matchID)
 
 
-def processTeamStats(stats):
+def processMatchStats(stats, matchID):
+    """
+
+    :param stats: A dictionary object which contains the output of matchStats() but is not yet cleaned
+    :return: a cleaned up pandas Dataframe, ready for writing to the database
+    """
     stats['tries'] = [t[0] for t in stats['Tries']]
     stats['conversion_attempts'], stats['conversion_goals'] = [c.split(' from') for c in stats['Conversion goals']]
     stats['penalty_attempts'], stats['penalty_goals'] = [c.split(' from') for c in stats['Penalty goals']]
     stats['yellow_cards'], stats['red_cards'] = [c.split('/') for c in stats['Yellow/red cards']]
-    stats['tackles_made'], stats['tackles_missed'] = [c.split('/') for c in stats['Tackles made/missed']]
+    stats['tackles_made'] = [c.split('/')[0] for c in stats['Tackles made/missed']]
+    stats['tackles_missed'] = [c.split('/')[1] for c in stats['Tackles made/missed']]
 
     stats['possession_overall'] = [s.split(' ')[0] for s in stats['Possession (1H/2H)']]
     stats['possession_1H'] = [s[s.find("(") + 1:s.find(")")].split('/')[0] for s in stats['Possession (1H/2H)']]
@@ -100,12 +120,29 @@ def processTeamStats(stats):
 
     for ky in ['Tries', 'Yellow/red cards', 'Tackles made/missed', 'Conversion goals', 'Penalty goals',
                'Possession (1H/2H)', 'Territory (1H/2H)', 'Scrums on own feed', 'Lineouts on own throw', 'Rucks won',
-               'Mauls won', 'Penalties conceded (Freekicks)']:
+               'Mauls won', 'Penalties conceded (Freekicks)', 'Tackling success rate']:
         stats.pop(ky, None)
-    return stats
+
+    # Produce a DataFrame, rename columns and turn all percentages to floats
+    stats = pd.DataFrame(stats)
+    stats['match_id'] = matchID
+    stats.columns = [col.replace(' ', '_') for col in stats.columns]
+    for col in ['Kick_at_goal_success', 'possession_overall', 'possession_1H', 'possession_2H', 'territory_overall',
+                'territory_1H', 'territory_2H']:
+        stats[col] = stats[col].str.replace('%', '').astype('float') / 100
+
+    return pd.DataFrame(stats)
 
 
 def teamStats(teamStats, matchID, nation):
+    """
+
+    :param teamStats: soup object which contains the table of team stats
+    :param matchID: the matchID, required for creating data
+    :param nation: the nation which this is the stats for
+    :return: returns a pandas dataframe object with the results
+    """
+    # creates a string which is then formatted into csv and read into a pandas DataFrame
     statString = []
     for row in teamStats.find_all('tr'):
         if not row.find('td', {'colspan': '14'}):
@@ -127,6 +164,7 @@ def teamStats(teamStats, matchID, nation):
     df['match_id'] = matchID
     df['nation'] = nation
 
+    # The selected fields are all separated by '/' and therefore split into multiple columns for cleaner data
     clean = {'T/A': ['tries', 'try_assists'],
              'K/P/R': ['kicks', 'passes', 'runs'],
              'Tack': ['tackles_made', 'tackles_missed'],
@@ -141,55 +179,74 @@ def teamStats(teamStats, matchID, nation):
             n += 1
     df.drop([k for k in clean.keys()], axis=1, inplace=True)
 
+    # Cleaned and processed dataframe is produced
     return df
 
 
-matchID = 300729
-file = f'../html/Rugby Union - ESPN Scrum - Wales v Ireland at Millennium Stadium_files/{matchID}.html'
-response = open(file).read()
-# response = requests.get(url).text
-soup = BeautifulSoup(response, 'lxml')
+def timelineDetails(timeline):
+    return timeline
 
-left = soup.find('div', {'id': 'liveLeft'})
-right = soup.find('div', {'id': 'liveRight'})
 
-ltabs = left.find_all('div', {'class': 'tabbertab'})
-rtabs = right.find_all('div', {'class': 'tabbertab'})
+def scrape(matchID):
+    file = f'../html/Rugby Union - ESPN Scrum - Wales v Ireland at Millennium Stadium_files/{matchID}.html'
+    response = open(file).read()
+    # response = requests.get(url).text
+    soup = BeautifulSoup(response, 'lxml')
 
-team = 0
-notesCheck = 0
-teamCheck = 0
-matchStatsCheck = 0
-teamStatsCheck = 0
+    left = soup.find('div', {'id': 'liveLeft'})
+    right = soup.find('div', {'id': 'liveRight'})
 
-# checks both sides as layouts vary
-for side in [ltabs, rtabs]:
+    ltabs = left.find_all('div', {'class': 'tabbertab'})
+    rtabs = right.find_all('div', {'class': 'tabbertab'})
 
-    for tab in side:
-        if tab.h2.text == 'Notes':
-            notes = readNotes(tab.find_all('tr'), matchID)
-            notesCheck += 1
-        elif tab.h2.text == 'Teams':
-            teams = readTeams(tab.find('table'))
-            teamCheck += 1
-        elif tab.h2.text == 'Match stats':
-            mStats = matchStats(tab.find('table'))
-            matchStatsCheck += 1
-        elif 'stats' in tab.h2.text:
-            nation = tab.h2.text.split(' ')[0]
-            tStats = teamStats(tab.find('table'), matchID, nation)
-            teamStatsCheck += 1
+    notesCheck = 0
+    teamCheck = 0
+    matchStatsCheck = 0
+    teamStatsCheck = 0
+    timeline = 0
 
-resultsLookup = {
-    'notesCheck': notesCheck,
-    'teamCheck': teamCheck,
-    'matchStatsCheck': matchStatsCheck,
-    'teamStatsCheck': teamStatsCheck
-}
+    # checks both sides as layouts vary
+    for side in [ltabs, rtabs]:
 
-print(resultsLookup)
+        for tab in side:
+            if tab.h2.text == 'Notes':
+                notes = readNotes(tab.find_all('tr'), matchID)
+                notesCheck += 1
+            elif tab.h2.text == 'Teams':
+                teamScores = readTeams(tab.find('table'))
+                teamCheck += 1
+            elif tab.h2.text == 'Match stats':
+                mStats = matchStats(tab.find('table'), teamScores, matchID)
+                matchStatsCheck += 1
+            elif tab.h2.text == 'Timeline':
+                timelineStats = tab.find('table')
 
-print(notes)
-print(teams)
-print(mStats)
-print(tStats)
+                timeline += 1
+            elif 'stats' in tab.h2.text:
+                nation = tab.h2.text.split(' ')[0]
+                if teamStatsCheck == 0:
+                    tStats = teamStats(tab.find('table'), matchID, nation)
+                else:
+                    tStats = pd.concat([tStats, teamStats(tab.find('table'), matchID, nation)])
+                teamStatsCheck += 1
+
+    # log of reads completed
+    resultsLookup = {'match_id': matchID,
+                     'notes_check': notesCheck,
+                     'timeline_check': timeline,
+                     'team_check': teamCheck,
+                     'match_stats_check': matchStatsCheck,
+                     'team_stats_check': teamStatsCheck
+                     }
+
+    # todo: write teamScores & timeline to db. Change the way the response is called
+    print(teamScores)
+    print(timeline)
+    executeQuery(createSQL(notes, 'match_notes'))
+    executeQuery(createSQL(resultsLookup, 'match_details_log'))
+    writeDataFrame(mStats, table='player_stats_match')
+    writeDataFrame(tStats, table='team_match_stats')
+
+# Run all the functions
+matchID = '300729'
+scrape(matchID)
